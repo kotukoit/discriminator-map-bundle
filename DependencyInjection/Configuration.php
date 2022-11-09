d1d6f33ccb8fd2ede3e8054811230f59db17d818
Aguzzi, Marcello <marcello.aguzzi@kotuko.it>
2022-11-09 17:54:28 +0100

    [UPDATE] fix deprecation adding TreeBuilder return type

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('ped_discriminator_map');
 
